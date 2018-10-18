93237643835a8b0b219c3467ebb339b6dc4610ab
Ela Korkmaz <elakorkmaz87@gmail.com>
2018-10-18 16:58:12 +0200

    react setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,24 @@
 const path = require("path");
-const HWP = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-    entry: path.join(__dirname, "/src/index.js"),
-    output: {
-        filename: "build.js",
-        path: path.join(__dirname, "/dist")},
-    module:{
-        rules:[{
-           test: /\.js$/,
-           exclude: /node_modules/,
-           loader: "babel-loader"
-        }]
-    },
-    plugins:[
-        new HWP(
-           {template: path.join(__dirname," /src/index.html")}
-        )
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index-bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
     ]
- };
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+};
