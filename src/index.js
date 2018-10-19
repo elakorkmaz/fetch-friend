b6dbe33b00cb617e15510139c2bbbe821a594664
Ela Korkmaz <elakorkmaz87@gmail.com>
2018-10-19 18:17:45 +0200

    api working

--- src/index.js
+++ src/index.js
@@ -1,10 +1,54 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => (
-   <div>
-      <h1>Hello world!</h1>
-   </div>
-)
+class BreedsList extends React.Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      breeds: []
+    };
+  }
+  
+  componentDidMount() {
+  fetch("https://dog.ceo/api/breeds/list")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.setState({
+          isLoaded: true,
+          breeds: result.message
+        });
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    );
+  }
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+  render() {
+    const { error, isLoaded, breeds } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {breeds.map(breed => (
+            <li key={breed}>
+              {breed}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
+  
+}
+
+ReactDOM.render(<BreedsList/>, document.getElementById("root"));
